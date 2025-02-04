16974bb4ff7dcb4fb720cf1cc5c006b6f203d3f3
Liz Thompson <elth8461@colorado.edu>
2025-02-04 10:44:42 -0700

    Set up constants for all functions to pull from

---
+++ getConst.m
@@ -0,0 +1,13 @@
+function [const] = getConst()
+%GETCONST Retrieve constants for all functions and return them in a
+%structure
+
+% Universal Constants
+const.gravity = 9.81; % m/s/s
+
+% Inputs
+const.h0Coaster = 100; % m
+const.stepSize = 0.1; % m
+
+end
+
