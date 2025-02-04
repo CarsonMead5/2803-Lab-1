31a4a6c7f8d8d3235488fe192e458e513dba02f5
Liz Thompson <elth8461@colorado.edu>
2025-02-04 11:32:36 -0700

    Update bankedTurnMain.m
    made 3 G-plots into 1 subplot. Made constants dependent on getConst return values

--- bankedTurnMain.m
+++ bankedTurnMain.m
@@ -4,9 +4,11 @@ clear; clc; close all;
 
 % Change step, inital height, and gravity to const
 
-velocityBT = 50; % m/s
-stepLength = 0.1; % m
-gravity = 9.81; % m/s/s
+const = getConst();
+
+velocityBT = sqrt( 2 * const.gravity * const.h0Coaster ); % m/s
+const.stepSize = 0.1; % m
+const.gravity = 9.81; % m/s/s
 
 % Inputs
 maxG = 5; % g's
@@ -14,7 +16,7 @@ lengthBT = 100; % m
 
 % Set-Up arrays for the position every step length and the corresponding
 % time values
-distance = stepLength:stepLength:lengthBT; % m
+distance = const.stepSize:const.stepSize:lengthBT; % m
 timeValues = distance / velocityBT; % s
 
 % Const for BT Angle
@@ -26,10 +28,15 @@ amplitudeBTAngle = maxBTAngle / 2; % rad
 
 % θ(t) = - (θmax/2) * cos((2pi/turnDuration)t) + (θmax/2)
 angle = bankedTurnTheta(amplitudeBTAngle,turnDuration,timeValues); % rad
-% plot(distance,angle)
+
+figure();
+plot(distance,angle)
+title('Bank Angle vs Distance')
+xlabel('Distance (m)')
+ylabel('Bank Angle (Rad)')
 
 % r = v^2/g * cot(θ(t))
-radius = bankedTurnRadius(velocityBT,gravity,angle);
+radius = bankedTurnRadius(velocityBT,const.gravity,angle);
 
 %{
 figure();
@@ -38,23 +45,22 @@ set(gca,'YScale','log')
 %}
 
 
-
-theta = stepLength ./ radius;
+theta = const.stepSize ./ radius;
 for i = 1:length(radius)
     x(i) = radius(i) .* cos( sum(theta(1:i)));
     y(i) = radius(i) .* sin(sum(theta(1:i)));
 end
 
-
 %{
 theta = distance ./ radius;
     x = radius .* cos(theta);
     y = radius .* sin(theta);
-%}
 
-% plot(x,y)
-% set(gca,'XScale','log')
+figure();
+plot(x,y)
+set(gca,'XScale','log')
 % set(gca,'YScale','log')
+%}
 
 %% Modelling Gs Along the Track
 
@@ -63,23 +69,20 @@ gForceLateral = 0 * distance;
 gForceNormal = 1 ./ cos( angle );
 
 figure();
+subplot(1,3,1)
 plot(distance,gForceForBack)
 title('Forwards/Backwards G-Forces')
 xlabel('Distance (m)')
 ylabel('G-Forces (Gs)')
 
-figure();
+subplot(1,3,2)
 plot(distance,gForceLateral)
 title('Lateral G-Forces')
 xlabel('Distance (m)')
 ylabel('G-Forces (Gs)')
 
-figure();
+subplot(1,3,3)
 plot(distance,gForceNormal)
 title('Normal G-Forces')
 xlabel('Distance (m)')
 ylabel('G-Forces (Gs)')
-
-
-
-
