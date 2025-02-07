143e91069bd8905c6845021de93a10ab7acbb69f
CarsonMead5 <Carson.Mead@colorado.edu>
2025-02-06 23:45:56 -0700

    Update brakingsection.m

--- brakingsection.m
+++ brakingsection.m
@@ -1,9 +1,8 @@
-clc
-clear
-close all
+function [G_updown,G_lateral,G_backwards,path_length] = coaster_breakingsection(h_0,inc)
 
 % Define parameters
-v0 = 50; % initial velocity in m/s 
+g = 9.81; % gravitational acceleration in m/s^2
+v0 = sqrt(2*g*h_0); % initial velocity in m/s 
 vf = 0; % final velocity in m/s
 height_initial = 0;
 height_final = 0;
@@ -13,43 +12,49 @@ db = (v0^2) / (2 * (a_maxb)); % distance traveled during braking
 g = 9.81; % gravitational acceleration in m/s^2
 
 % Calculate position along the braking section
-path_length = linspace(0, db, 100); % path length from 0 to db
+path_length = 0:inc:db; % path length from 0 to db
 G_lateral = 0 * ones(size(path_length));
-G_updown = 0 * ones(size(path_length));
+G_updown = 1 * ones(size(path_length));
 G_backwards = (a_maxb / g) * ones(size(path_length)); % constant backward G's
 
+% Creating figure with subplots
+figure()
+sgtitle("Breaking Section Loop G-Forces")
 
-% Create figure and subplots
-figure;
+subplot(3,1,1)
+hold on
+plot(path_length,G_updown,'b',LineWidth=1.5)
+yline(6,'r')
+yline(-1,'r')
+hold off
+title("Normal G-Force vs. Distance")
+xlabel("Distance (m)")
+ylabel("G-Force")
+xlim([0,path_length(end)])
+ylim([-2,7])
 
-% Create the subplot for Backward G's during the braking section
-subplot(3,1,1);
-plot(path_length, G_lateral, 'k', 'LineWidth', 2);
-title('Lateral Gs During Braking Section');
-xlabel('Path Length along Track (m)');
-ylabel('Lateral G (g)');
-ylim([-5 5]); % G limits
-grid on;
+subplot(3,1,2)
+hold on
+plot(path_length,G_lateral,'b',LineWidth=1.5)
+yline(3,'r')
+yline(-3,'r')
+hold off
+title("Lateral G-Force vs. Distance")
+xlabel("Distance (m)")
+ylabel("G-Force")
+xlim([0,path_length(end)])
+ylim([-4,4])
 
-subplot(3,1,2);
-plot(path_length, G_updown, 'k', 'LineWidth', 2);
-title('Up/Down Gs During Braking Section');
-xlabel('Path Length along Track (m)');
-ylabel('Up/Down G (g)');
-ylim([-5 5]); % G limits
-grid on;
+subplot(3,1,3)
+hold on
+plot(path_length,G_backwards,'b',LineWidth=1.5)
+yline(5,'r')
+yline(-4,'r')
+hold off
+title("Tangential G-Force vs. Distance")
+xlabel("Distance (m)")
+ylabel("G-Force")
+xlim([0,path_length(end)])
+ylim([-5,6])
 
-subplot(3,1,3);
-plot(path_length, G_backwards, 'k', 'LineWidth', 2);
-title('Backward Gs During Braking Section');
-xlabel('Path Length along Track (m)');
-ylabel('Backward G (g)');
-ylim([0 10]); % G limits
-grid on;
-
-
-% Formatting
-sgtitle('G-Forces during Braking Section');
-
-% Display the figure
-figure(gcf);
+print('breakingsection_gforces_2803_lab1','-dpng','-r300') %saves image file (png)
