3d4bc41deb67742f9ae0f223336a3a100e012cef
Liz Thompson <elth8461@colorado.edu>
2025-02-10 19:36:37 -0700

    Delete bankedTurnTheta.m
    Re-Do Banked Turn Code

--- bankedTurnTheta.m
+++
@@ -1,8 +0,0 @@
-function [bankAngle] = bankedTurnTheta(amplitudeBTAngle,turnDuration,timeValues)
-%BANKEDTURNANGLE Values of Theta over the duration of the turn
-
-for i = 1:length(timeValues)
-    bankAngle(i) = - amplitudeBTAngle * cos( (2*pi/turnDuration) * timeValues(i) ) + amplitudeBTAngle;
-end
-
-end
