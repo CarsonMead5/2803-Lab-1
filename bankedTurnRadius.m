1a09628a538db357efb359d436d8b027def4ff98
Liz Thompson <elth8461@colorado.edu>
2025-02-10 19:36:27 -0700

    Delete bankedTurnRadius.m
    Re-Do Banked Turn Code

--- bankedTurnRadius.m
+++
@@ -1,9 +0,0 @@
-function [bankRadius] = bankedTurnRadius(velocityBT,gravity,angle)
-%BANKEDTURNRADIUS Summary of this function goes here
-
-for i = 1:length(angle)
-    bankRadius(i) = velocityBT^2 / gravity * cot(angle(i));
-end
-
-end
-
